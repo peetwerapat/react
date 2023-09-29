85ef05074ad1a88311a6a4391d9863a8e06542c7
peetwerapat <werapatsaejong@gmail.com>
2023-09-29 15:41:21 +0700

    navbar login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Greeting from './components/Greeting'
+import NavbarLogin from './components/NavbarLogin'
 
 function App() {
   return (
     <div className="App">
+      <NavbarLogin />
       <Greeting />
     </div>
   )
---
+++ src/components/NavbarLogin.tsx
@@ -0,0 +1,12 @@
+import classes from './NavbarLogin.module.css'
+
+const NavbarLogin = () => {
+  return (
+    <div className={classes.top}>
+      <h3>Navbar</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default NavbarLogin
